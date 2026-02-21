6f2157bedb8ffefea514550e19b884816da8f1ad
BBee98 <blue98.bee.dev@gmail.com>
2026-02-21 11:59:39 +0100

    feat: add docs. Now can get components from declared folder at envs

--- ssr/constants.js
+++ ssr/constants.js
@@ -2,3 +2,6 @@ export const ENVIRONMENTS = [
     "REPOSITORY_COMPONENTS_FOLDER",
     "REPOSITORY_COMPONENTS_FILE"
 ]
+
+export const EXTENSION_JS_FILES = [".js", ".jsx", ".ts", ".tsx"];
+export const EXTENSION_CSS_FILES = [".css"];
